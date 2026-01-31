211eef61fd3d612ca7d17412dbcf2802c1dc5ce3
abelleisle <abelleisle@protonmail.com>
2026-01-31 13:11:03 -0500

    Cleaned up build.zig and added client + net modules

---
+++ lib/net/root.zig
@@ -0,0 +1,11 @@
+//--------------------------------------------------
+// Net Exports
+
+//--------------------------------------------------
+// Local imports
+const std = @import("std");
+
+//--------------------------------------------------
+// Test references
+// This ensures all tests in these files are run when executing `zig build test`
+test {}
